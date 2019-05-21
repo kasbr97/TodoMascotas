67f1d7c9ea1c694c27c27062d96d68b410240268
Kevin Burga <kev.asbr@gmail.com>
2019-05-21 14:05:44 -0500

    Add files via upload

---
+++ mascotas/src/main/java/com/todomascotas/mascotas/WebMvcConfiguration.java
@@ -0,0 +1,10 @@
+package com.todomascotas.mascotas;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer{
+
+	
+}
---
+++ mascotas/src/test/java/com/todomascotas/mascotas/MascotasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.todomascotas.mascotas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MascotasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
